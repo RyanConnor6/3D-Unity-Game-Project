45051808c3c85b5db8858c1d4f5fe50cae653bcd
Ryan Connor <40437041@live.napier.ac.uk>
2022-02-21 13:38:02 +0000

    Colour System Implemented
    Added colour changing scripts. Testable in the test room with all 3 settings (2 colours, 3 colours, 4 colours.) System in place to alter time between switches if needed.

---
+++ Group Project Game/Assets/Scripts/PlatformScripts/RedScript.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RedScript : MonoBehaviour
+{
+    public Material material1;
+    public Material material2;
+    public Renderer rend;
+    public GameObject timer;
+    public float time;
+    public float numberOfColours;
+    public float timeBetweenSwitches;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        StartCoroutine(ColourCycle());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator ColourCycle()
+    {
+        while (true)
+        {
+            gameObject.GetComponent<BoxCollider>().enabled = true;
+            rend.material = material2;
+            yield return new WaitForSeconds(timeBetweenSwitches);
+
+            gameObject.GetComponent<BoxCollider>().enabled = false;
+            rend.material = material1;
+            yield return new WaitForSeconds(timeBetweenSwitches);
+
+            if (numberOfColours > 2)
+            {
+                yield return new WaitForSeconds(timeBetweenSwitches);
+                if (numberOfColours == 4)
+                {
+                    yield return new WaitForSeconds(timeBetweenSwitches);
+                }
+            }
+        }
+    }
+}
