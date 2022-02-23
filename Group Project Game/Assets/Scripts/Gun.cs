7b4bdd45a5beadd3e1211958a90639064bbcee60
Ryan Connor <40437041@live.napier.ac.uk>
2022-02-23 01:20:47 +0000

    Changing To Better Shooting System

---
+++ Group Project Game/Assets/Scripts/Gun.cs
@@ -0,0 +1,156 @@
+using System;
+using System.Diagnostics;
+using UnityEngine;
+using System.Collections;
+
+public class Gun : MonoBehaviour
+{
+    //Gun variables
+    public float damage = 10f;
+    public float range = 100f;
+    public float fireRate = 150f;
+    public float impactForce = 30f;
+
+    //FX and other variables
+    public Camera fpscamera;
+    public ParticleSystem muzzleflash;
+    public GameObject impactEffect;
+    public Transform BulletSpawnPoint;
+
+    //Bullet trail
+    [SerializeField]
+    private TrailRenderer BulletTrail;
+
+    //Bullet spread
+    [SerializeField]
+    private Vector3 BulletSpreadVariance = new Vector3(0.1f, 0.1f, 0.1f);
+
+    //Next possible shoot time
+    private float nextTimeToFire = 0f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Shoot when mouse1 pressed and shoot delay is over, reset timer
+        //Pistol shoot mode
+        if (Input.GetButton("Fire1") && Time.time >= nextTimeToFire)
+        {
+            nextTimeToFire = Time.time + 1f / fireRate;
+            PistolShoot();
+        }
+        //Shoot when mouse2 pressed and shoot delay is over, reset timer
+        //Pistol shoot mode
+        if (Input.GetButton("Fire2") && Time.time >= nextTimeToFire)
+        {
+            nextTimeToFire = Time.time + 1f / fireRate;
+            ShotgunShoot();
+        }
+
+    }
+
+    //Pistol shoot
+    void PistolShoot()
+    {
+        //Play shoot FX
+        muzzleflash.Play();
+
+        //Raycast
+        RaycastHit hit;
+
+        //Bullet raycast
+        if (Physics.Raycast(fpscamera.transform.position, fpscamera.transform.forward, out hit, range))
+        {
+            //Get hit object
+            UnityEngine.Debug.Log(hit.transform.name);
+            Target target = hit.transform.GetComponent<Target>();
+
+            //If its a target send damage to it
+            if (target != null)
+            {
+                target.TakeDamage(damage);
+            }
+            if (hit.rigidbody != null)
+            {
+                //hit.rigidbody.AddForce(-hit.normal * impactForce);
+            }
+
+            //Impact FX
+            GameObject impactGO = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+            Destroy(impactGO, 2f);
+
+            //Trail FX
+            TrailRenderer trail = Instantiate(BulletTrail, BulletSpawnPoint.position, Quaternion.identity);
+            StartCoroutine(SpawnTrail(trail, hit));
+        }
+
+    }
+
+    void ShotgunShoot()
+    {
+        //Play shoot FX
+        muzzleflash.Play();
+
+        //Raycast
+        RaycastHit hit;
+
+        //10 bullets
+        for (int i = 0; i < 9; i++)
+        {
+            //Direction
+            Vector3 direction = fpscamera.transform.forward;
+
+            //Add spread
+            direction += new Vector3(
+                UnityEngine.Random.Range(-BulletSpreadVariance.x, BulletSpreadVariance.x),
+                UnityEngine.Random.Range(-BulletSpreadVariance.y, BulletSpreadVariance.y),
+                UnityEngine.Random.Range(-BulletSpreadVariance.z, BulletSpreadVariance.z)
+                );
+
+            direction.Normalize();
+
+            //Bullet raycast
+            if (Physics.Raycast(fpscamera.transform.position, direction, out hit, range))
+            {
+                //Get hit object
+                UnityEngine.Debug.Log(hit.transform.name);
+                Target target = hit.transform.GetComponent<Target>();
+
+                //If its a target send damage to it
+                if (target != null)
+                {
+                    target.TakeDamage(damage);
+                }
+                if (hit.rigidbody != null)
+                {
+                    //hit.rigidbody.AddForce(-hit.normal * impactForce);
+                }
+
+                //Impact FX
+                GameObject impactGO = Instantiate(impactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+                Destroy(impactGO, 2f);
+
+                //Trail FX
+                TrailRenderer trail = Instantiate(BulletTrail, BulletSpawnPoint.position, Quaternion.identity);
+                StartCoroutine(SpawnTrail(trail, hit));
+            }
+        }
+    }
+
+    //Make bullet trail
+    private IEnumerator SpawnTrail(TrailRenderer Trail, RaycastHit Hit)
+    {
+        float time = 0;
+        Vector3 startPosition = Trail.transform.position;
+
+        while (time < 1)
+        {
+            Trail.transform.position = Vector3.Lerp(startPosition, Hit.point, time);
+            time += Time.deltaTime / Trail.time;
+
+            yield return null;
+        }
+        Trail.transform.position = Hit.point;
+
+        Destroy(Trail.gameObject, Trail.time);
+    }
+}
---
+++ Group Project Game/Assets/Scripts/Target.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Target : MonoBehaviour
+{
+    //Health and max health
+    public float health = 50f;
+    public float maxHealth = 50f;
+
+    //UI elements
+    public GameObject healthBarUI;
+    public Slider slider;
+
+    void Start()
+    {
+        //Instantiate
+        health = maxHealth;
+        slider.value = CalculateHealth();
+    }
+
+    void Update()
+    {
+        //Correct slider value
+        slider.value = CalculateHealth();
+
+        //Keep UI active
+        if (health < maxHealth)
+        {
+            healthBarUI.SetActive(true); 
+        }
+
+        //Destroy when no health
+        if (health <= 0)
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    //Take damage from bullet
+    public void TakeDamage(float amount)
+    {
+        health -= amount;
+    }
+
+    //Get health for slider
+    float CalculateHealth()
+    {
+        return health / maxHealth;
+    }
+}
