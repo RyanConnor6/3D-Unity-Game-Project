63a7327bfdce1319217d7c04ff1b5ae5a5594dd6
Ryan Connor <40437041@live.napier.ac.uk>
2022-03-01 13:27:28 +0000

    Adding Scripts For Moving Colours

---
+++ Group Project Game/Assets/Scripts/PlatformScripts/MovingBlue.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingBlue : MonoBehaviour
+{
+    public Material InactiveMaterial;
+    public Material ActiveMaterial;
+    private Renderer rend;
+    public GameObject sceneController;
+    private float time;
+    private float numberOfColours;
+    private float timeBetweenSwitches;
+    public GameObject Player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        numberOfColours = sceneController.GetComponent<SceneController>().numberOfColours;
+        timeBetweenSwitches = sceneController.GetComponent<SceneController>().timeBetweenSwitches;
+        StartCoroutine(FirstCycle());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    IEnumerator FirstCycle()
+    {
+        SetAllCollidersStatus(false);
+        rend.material = InactiveMaterial;
+        yield return new WaitForSeconds(timeBetweenSwitches);
+
+        SetAllCollidersStatus(true);
+        rend.material = ActiveMaterial;
+        yield return new WaitForSeconds(timeBetweenSwitches);
+        StartCoroutine(ColourCycle());
+    }
+
+    IEnumerator ColourCycle()
+    {
+        while (true)
+        {
+            SetAllCollidersStatus(false);
+            rend.material = InactiveMaterial;
+            yield return new WaitForSeconds(timeBetweenSwitches);
+
+            if (numberOfColours > 2)
+            {
+                yield return new WaitForSeconds(timeBetweenSwitches);
+                if (numberOfColours == 4)
+                {
+                    yield return new WaitForSeconds(timeBetweenSwitches);
+                }
+            }
+
+            SetAllCollidersStatus(true);
+            rend.material = ActiveMaterial;
+            yield return new WaitForSeconds(timeBetweenSwitches);
+        }
+    }
+
+    public void SetAllCollidersStatus(bool active)
+    {
+        foreach (Collider c in GetComponents<Collider>())
+        {
+            c.enabled = active;
+            Player.transform.parent = null;
+        }
+    }
+}
