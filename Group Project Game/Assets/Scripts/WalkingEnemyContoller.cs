d2a87274ab5b5bf7c427e5d22a8d29d3e16ac846
petertheverner <peterverner@sky.com>
2022-03-02 09:31:13 +0000

    Moving enemy script added
    Enemy added script for a chasing enemy that will attack when in the players vicinity. This will act as a base for future enemy types and scripts

---
+++ Group Project Game/Assets/Scripts/WalkingEnemyContoller.cs
@@ -0,0 +1,61 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class WalkingEnemyContoller : MonoBehaviour
+{
+
+	public float lookradius = 10f;
+
+    Transform target;
+    NavMeshAgent agent;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //sets the enmys target as the player
+        target = PlayerManager.instance.player.transform;
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    { //distance between player and enemy
+        float distance = Vector3.Distance(target.position, transform.position);
+
+        //if the distance is less than the look radius then the enemy will chase the player
+        if (distance <= lookradius)
+        {
+            agent.SetDestination(target.position);
+        }
+        // If within attacking distance
+        //if (distance <= agent.stoppingDistance)
+        //{
+        //    CharacterStats targetStats = target.GetComponent<CharacterStats>();
+        //    if (targetStats != null)
+        //    {
+        //        combat.Attack(targetStats);
+        //    }
+        { 
+            FaceTarget();   // Make sure to face towards the target
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        //shows the radius of what the enemy character can see
+        Gizmos.color = Color.green;
+        Gizmos.DrawWireSphere(transform.position, lookradius);
+    }
+
+
+    // Rotate to face the target
+    void FaceTarget()
+    {
+        Vector3 direction = (target.position - transform.position).normalized;
+        Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
+        transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * 5f);
+    }
+
+}
